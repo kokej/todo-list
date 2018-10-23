da49dcef07c8359e2d770bbd696d2717daf941da
kokej <jamarqueza@gmail.com>
2018-10-23 21:47:21 +0200

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
-
-const app = express();
+var app = express();
 
 app.use(express.static(__dirname + '/dist/todo-list'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/todo-list/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/todo-list/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+    console.log('server listening on port ' + port);
+});
+
+module.exports = app;
